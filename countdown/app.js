c7490dab367e2d4bc2e07c2f84159e0bec250fd4
King-fReeBoDy <89941338+King-fReeBoDy@users.noreply.github.com>
2023-10-09 15:35:59 +0000

    countdown

--- countdown/app.js
+++ countdown/app.js
@@ -1,8 +1,14 @@
+const dayEl = document.getElementById("day");
+const hourEl = document.getElementById("hour");
 const minuteEl = document.getElementById("minute");
 const secondEl = document.getElementById("second");
 const deadline = document.getElementById("deadline");
 
-const futureTime = Date.now() + 1000 * 5;
+const inputSecond = 1000;
+const inputMinute = inputSecond * 60;
+const inputHour = inputMinute * 60;
+const inputDay = inputHour * 24;
+const futureTime = Date.now() + inputDay;
 let timeId;
 
 const countDown = () => {
@@ -14,11 +20,17 @@ const countDown = () => {
   const hour = minute * 60;
   const day = hour * 24;
 
+  const textDay = Math.floor((timeDifference % day) / day);
+  const textHour = Math.floor((timeDifference % day) / hour);
   const textMinute = Math.floor((timeDifference % hour) / minute);
   const textSecond = Math.floor((timeDifference % minute) / second);
 
-  minuteEl.textContent = textMinute.toString().padStart(2, "0");
-  secondEl.textContent = textSecond.toString().padStart(2, "0");
+  dayEl.textContent = textDay.toString().padStart(2, "0") + " " + "Day";
+  hourEl.textContent = textHour.toString().padStart(2, "0") + " " + "Hour";
+  minuteEl.textContent =
+    textMinute.toString().padStart(2, "0") + " " + "Minutes";
+  secondEl.textContent =
+    textSecond.toString().padStart(2, "0") + " " + "Seconds";
   if (textMinute === 0 && textSecond === 0) {
     clearInterval(timeId);
     deadline.textContent = "Time elapse";
