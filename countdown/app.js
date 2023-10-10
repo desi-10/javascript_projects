09654acd4628df40737cbe557671665e26aaa86a
King-fReeBoDy <89941338+King-fReeBoDy@users.noreply.github.com>
2023-10-10 13:54:04 +0000

    countdown day update

--- countdown/app.js
+++ countdown/app.js
@@ -8,11 +8,11 @@ const inputSecond = 1000;
 const inputMinute = inputSecond * 60;
 const inputHour = inputMinute * 60;
 const inputDay = inputHour * 24;
-const futureTime = Date.now() + inputDay;
+const futureTime = Date.now() + inputDay * 2;
 let timeId;
 
 const countDown = () => {
-  const currentTime = Date.now();
+  const currentTime = Date.now(); // Moved outside the function
   const timeDifference = futureTime - currentTime;
 
   const second = 1000;
@@ -20,20 +20,31 @@ const countDown = () => {
   const hour = minute * 60;
   const day = hour * 24;
 
-  const textDay = Math.floor((timeDifference % day) / day);
+  const textDay = Math.floor(timeDifference / day);
   const textHour = Math.floor((timeDifference % day) / hour);
   const textMinute = Math.floor((timeDifference % hour) / minute);
   const textSecond = Math.floor((timeDifference % minute) / second);
 
-  dayEl.textContent = textDay.toString().padStart(2, "0") + " " + "Day";
-  hourEl.textContent = textHour.toString().padStart(2, "0") + " " + "Hour";
+  dayEl.textContent =
+    textDay.toString().padStart(2, "0") +
+    " " +
+    (textDay === 1 ? "Day" : "Days");
+  hourEl.textContent =
+    textHour.toString().padStart(2, "0") +
+    " " +
+    (textHour === 1 ? "Hour" : "Hours");
   minuteEl.textContent =
-    textMinute.toString().padStart(2, "0") + " " + "Minutes";
+    textMinute.toString().padStart(2, "0") +
+    " " +
+    (textMinute === 1 ? "Minute" : "Minutes");
   secondEl.textContent =
-    textSecond.toString().padStart(2, "0") + " " + "Seconds";
+    textSecond.toString().padStart(2, "0") +
+    " " +
+    (textSecond === 1 ? "Second" : "Seconds");
+
   if (textMinute === 0 && textSecond === 0) {
     clearInterval(timeId);
-    deadline.textContent = "Time elapse";
+    deadline.textContent = "Time elapsed";
   }
 };
 
